2f8cc7620210a8f856e6b7de40e34d1490fad0da
unknown <eliteyasin1@gmail.com>
2026-02-16 11:33:21 +0500

    added service worker to worker call

--- src/index.js
+++ src/index.js
@@ -8,14 +8,17 @@ const { worker } = require('./mocks/browser');
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
-worker.start().then(() => {
+worker.start({
+  serviceWorker: {
+    url: '/router-project/mockServiceWorker.js'
+  }
+}).then(() => {
   root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
   );
 });
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
